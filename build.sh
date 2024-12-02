3d8f0781719193c2e8213a1c97a7d99a13a3f65f
Armin Schrenk <armin.schrenk@skymatic.de>
2024-12-02 16:35:39 +0100

    repair buildscript

--- build.sh
+++ build.sh
@@ -1,8 +1,25 @@
-read -p "Enter the full qualified JDK version (e.g. 22.0.2+9)" JDK_FULL_VERSION
+#!/bin/bash
+set -euo pipefail
 
-JDK_MAJOR_VERSION= ${JDK_FULL_VERSION:0:2}
+read -p $'Enter the full qualified JDK version (e.g. 22.0.2+9)\n' JDK_FULL_VERSION
+
+JDK_MAJOR_VERSION=${JDK_FULL_VERSION:0:2}
 PKG_NAME_AND_VERSION="openjdk-${JDK_MAJOR_VERSION}_${JDK_FULL_VERSION}"
-read -p "Enter the JDK tarball URL to download" JDK_TARBALL_URL
+read -p $'Enter the JDK tarball URL to download\n' JDK_TARBALL_URL
+echo "Downloading JDK source now..."
+curl -L ${JDK_TARBALL_URL} -o ${PKG_NAME_AND_VERSION}.orig.tar.gz
+
+cat << EOF
+Did you installed the following packages?
+* devscripts
+* debhelper
+EOF
+
+read -p $'If so, enter \"yes\"\n' READY_TO_BUILD
+if [ "yes" != ${READY_TO_BUILD} ]; then
+    echo "Missing packages required for building. Aborting build."
+    exit 1
+fi
 
 cat << EOF
 Did you updated the following files?
@@ -11,15 +28,14 @@ Did you updated the following files?
 * openjdk-${JDK_MAJOR_VERSION}/copyright,
 EOF
 
-read -p "If so, enter \"yes\"" READY_TO_BUILD
-if [ "yes" != ${READY_TO_BUILD}]; then
-    echo "Answer was not \"yes\". Aborting build.
+read -p $'If so, enter \"yes\"\n' READY_TO_BUILD
+if [ "yes" != ${READY_TO_BUILD} ]; then
+    echo "Answer was not \"yes\". Aborting build."
     exit 1
 fi
- 
-curl -L ${JDK_TARBALL_URL} -o ${PKG_NAME_AND_VERSION}.orig.tar.gz
+
+
 mkdir ${PKG_NAME_AND_VERSION} 
 tar -xzf ./${PKG_NAME_AND_VERSION}.orig.tar.gz -C ${PKG_NAME_AND_VERSION} --strip-components=1
-cp -R openjdk-${JDK_MAJOR_VERSION}/debian/ ${PKG_NAME_AND_VERSION} 
-cd ${PKG_NAME_AND_VERSION}
-debuild -us -uc -d -b
+cp -R openjdk-${JDK_MAJOR_VERSION}/debian ${PKG_NAME_AND_VERSION} 
+(cd ${PKG_NAME_AND_VERSION} && exec debuild -us -uc -d -b)
