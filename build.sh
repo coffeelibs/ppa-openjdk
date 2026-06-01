9517201acf33dba40161923f3dfb79f9e8cc61a0
Armin Schrenk <armin.schrenk@skymatic.de>
2026-06-01 17:56:17 +0200

    Add jdk 26.0.1+8 build
    Signed-off-by: Armin Schrenk <armin.schrenk@skymatic.de>

--- build.sh
+++ build.sh
@@ -1,13 +1,64 @@
 #!/bin/bash
 set -euo pipefail
 
-read -p $'Enter the full qualified JDK version (e.g. 22.0.2+9)\n' JDK_FULL_VERSION
+JDK_FULL_VERSION=""
+
+usage() {
+    cat << EOF
+Usage: $0 [--jdk-version <version>]
+
+Options:
+  --jdk-version <version>  Full qualified JDK version, e.g. 26.0.1+8
+  -h, --help              Show this help message
+EOF
+}
+
+while [ "$#" -gt 0 ]; do
+    case "$1" in
+        --jdk-version)
+            if [ "$#" -lt 2 ] || [ -z "$2" ]; then
+                echo "Missing value for --jdk-version"
+                usage
+                exit 1
+            fi
+            JDK_FULL_VERSION="$2"
+            shift 2
+            ;;
+        --jdk-version=*)
+            JDK_FULL_VERSION="${1#--jdk-version=}"
+            if [ -z "${JDK_FULL_VERSION}" ]; then
+                echo "Missing value for --jdk-version"
+                usage
+                exit 1
+            fi
+            shift
+            ;;
+        -h|--help)
+            usage
+            exit 0
+            ;;
+        *)
+            echo "Unknown argument: $1"
+            usage
+            exit 1
+            ;;
+    esac
+done
+
+if [ -z "${JDK_FULL_VERSION}" ]; then
+    read -r -p $'Enter the full qualified JDK version (e.g. 22.0.2+9)\n' JDK_FULL_VERSION
+fi
+
+if [ -z "${JDK_FULL_VERSION}" ]; then
+    echo "JDK version is required. Aborting build."
+    exit 1
+fi
 
 JDK_MAJOR_VERSION=${JDK_FULL_VERSION:0:2}
 PKG_NAME_AND_VERSION="openjdk-${JDK_MAJOR_VERSION}_${JDK_FULL_VERSION}"
-read -p $'Enter the JDK tarball URL to download\n' JDK_TARBALL_URL
+read -r -p $'Enter the JDK tarball URL to download\n' JDK_TARBALL_URL
 echo "Downloading JDK source now..."
-curl -L ${JDK_TARBALL_URL} -o ${PKG_NAME_AND_VERSION}.orig.tar.gz
+curl -L "${JDK_TARBALL_URL}" -o "${PKG_NAME_AND_VERSION}.orig.tar.gz"
 
 cat << EOF
 Did you installed the following packages?
@@ -17,8 +68,8 @@ Did you installed the following packages?
 * debhelper
 EOF
 
-read -p $'If so, enter \"yes\"\n' READY_TO_BUILD
-if [ "yes" != ${READY_TO_BUILD} ]; then
+read -r -p $'If so, enter \"yes\"\n' READY_TO_BUILD
+if [ "yes" != "${READY_TO_BUILD}" ]; then
     echo "Missing packages required for building. Aborting build."
     exit 1
 fi
@@ -30,14 +81,14 @@ Did you updated the following files?
 * openjdk-${JDK_MAJOR_VERSION}/copyright,
 EOF
 
-read -p $'If so, enter \"yes\"\n' READY_TO_BUILD
-if [ "yes" != ${READY_TO_BUILD} ]; then
+read -r -p $'If so, enter \"yes\"\n' READY_TO_BUILD
+if [ "yes" != "${READY_TO_BUILD}" ]; then
     echo "Answer was not \"yes\". Aborting build."
     exit 1
 fi
 
-rm --recursive --force ${PKG_NAME_AND_VERSION}
-mkdir ${PKG_NAME_AND_VERSION} 
-tar -xzf ./${PKG_NAME_AND_VERSION}.orig.tar.gz -C ${PKG_NAME_AND_VERSION} --strip-components=1
-cp -R openjdk-${JDK_MAJOR_VERSION}/debian ${PKG_NAME_AND_VERSION} 
-(cd ${PKG_NAME_AND_VERSION} && exec debuild -us -uc -d -b)
+rm --recursive --force "${PKG_NAME_AND_VERSION}"
+mkdir "${PKG_NAME_AND_VERSION}"
+tar -xzf "./${PKG_NAME_AND_VERSION}.orig.tar.gz" -C "${PKG_NAME_AND_VERSION}" --strip-components=1
+cp -R "openjdk-${JDK_MAJOR_VERSION}/debian" "${PKG_NAME_AND_VERSION}"
+(cd "${PKG_NAME_AND_VERSION}" && exec debuild -us -uc -d -b)
